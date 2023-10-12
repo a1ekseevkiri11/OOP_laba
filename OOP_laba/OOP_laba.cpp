3b1d768c9dceeb42158ff75862e1c807cb712b9d
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2023-10-13 00:01:07 +0500

    2 laba

--- OOP_laba/OOP_laba.cpp
+++ OOP_laba/OOP_laba.cpp
@@ -50,7 +50,7 @@ public:
 	void randomCoordinate() {
 		x = randomPosition();
 		y = randomPosition();
-	}	
+	}
 
 };
 
@@ -58,13 +58,13 @@ public:
 double getDistanceBetweenCenters(Circle circle1, Circle circle2) {
 	return sqrt(pow(circle1.getX() - circle2.getX(), 2) + pow(circle1.getY() - circle2.getY(), 2));
 }
-	
+
 //5
 static bool touchOnePoint(Circle circle1, Circle circle2) {
 	if (circle1.getRadius() + circle2.getRadius() == getDistanceBetweenCenters(circle1, circle2)) {
 		return true;
 	}
-	if (getDistanceBetweenCenters(circle1, circle2) + min(circle1.getRadius(), circle2.getRadius()) == max(circle1.getRadius(), circle2.getRadius())){
+	if (getDistanceBetweenCenters(circle1, circle2) + min(circle1.getRadius(), circle2.getRadius()) == max(circle1.getRadius(), circle2.getRadius())) {
 		return true;
 	}
 	return false;
@@ -92,17 +92,148 @@ public:
 	}
 };
 
-//2
+//2, 4
 class Pet {
 private:
 	int weight, age;
 	char sex;
 public:
-	Pet(int weight, char sex, int age): weight(weight), age(age), sex(sex){}
+	Pet(int weight, char sex, int age) : weight(weight), age(age), sex(sex) {}
+
+	int getWeight() {
+		return weight;
+	}
+
+	int getAge() {
+		return age;
+	}
+
+	char getSex() {
+		return sex;
+	}
+};
+
+
+class Cat : public Pet {
+private:
+	string name;
+public:
+	Cat(int weight, char sex, int age, string name) : Pet(weight, sex, age), name(name) {}
+
+	string getName() {
+		return name;
+	}
+
+	void getInfo() {
+		cout << "cat" << " " << getName() << " " << getAge() << " " << getSex() << " " << getWeight();
+	}
+};
+
+class Dog : public Pet {
+private:
+	string name;
+public:
+	Dog(int weight, char sex, int age, string name) : Pet(weight, sex, age), name(name) {}
+
+	string getName() {
+		return name;
+	}
+
+	void getInfo() {
+		cout << "dog" << " " << getName() << " " << getAge() << " " << getSex() << " " << getWeight();
+	}
+};
+
+
+//3
+class Fish {
+private:
+	string name;
+public:
+	Fish(string name) : name(name) {}
+
+	string getName() {
+		return name;
+	}
+
+	void swim() {
+		cout << getName() << " swim";
+	}
+};
+
+class Animal : public Fish {
+private:
+	int countLeg;
+public:
+	Animal(string name, int countLeg) : Fish(name), countLeg(countLeg) {}
+
+	int getCountLeg() {
+		return countLeg;
+	}
+
+	void walk() {
+		cout << getName() << " walk";
+	}
+};
+
+class Ape : public Animal {
+private:
+
+public:
+	Ape(string name, int countLeg) : Animal(name, countLeg) {}
+
+	void climbTree() {
+		cout << getName() << " climb tree";
+	}
+};
+
+class Human : public Ape {
+private:
+	bool bipedalism;
+public:
+	Human(string name, int countLeg, bool bipedalism) : Ape(name, countLeg), bipedalism(bipedalism){}
+
+	bool getBipedalism() {
+		return bipedalism;
+	}
+
+	void say() {
+		cout << getName() << " say";
+	}
+};
+
+
+//3 laba
+//полиморфизм
+class Cow {
+private:
+	string name;
+public:
+	Cow(string name) : name(name) {}
+
+	string getName() {
+		return name;
+	}
+
+	virtual void sayMyName() {
+		cout << "I'm cow - " << name << "\n";
+	}
+};
+
+class Whale : public Cow {
+public:
+	Whale(string name) : Cow(name) {}
+
+	virtual void sayMyName() {
+		cout << "I'm not a cow, I'm a whale -" << getName() << "\n";
+	}
 };
 
 int main() {
 	srand(time(NULL));//функция для разного рандома
-	Pegas pega1("loshadka");
-	pega1.flight();
+
+	Cow korova("Burenka");
+	Whale kit("Lui");
+	korova.sayMyName();
+	kit.sayMyName();
 }
